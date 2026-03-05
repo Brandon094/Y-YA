ae67895c22e5b7e5d26de644de7c2ce5c7351543
ChopCode <87330304+Brandon094@users.noreply.github.com>
2026-03-04 22:13:21 -0500

    cambio de estructura de las vistas se crearon componentes parareutilizarlos, y se usa en el 'index.php' los componentes creados.

---
+++ frontend/navbar.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<body>
+
+    <!-- NAVBAR -->
+    <header class="navbar">
+        <div class="logo">MiPanel</div>
+        <div class="user-info">
+            <span>Bienvenido, Usuario</span>
+        </div>
+    </header>
+
+</body>
+
+</html>
