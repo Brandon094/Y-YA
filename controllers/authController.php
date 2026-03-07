a595985cd20b4ea51794928678eca7a8035fa89b
ChopCode <87330304+Brandon094@users.noreply.github.com>
2026-03-06 22:15:46 -0500

    Se agregaron comentarios para describir las funciones y se agrego la funcion de logout para cerrar la session

--- controllers/authController.php
+++ controllers/authController.php
@@ -2,7 +2,9 @@
 include_once './model/authModel.php';
 
 class AuthController{
-
+    /*** Login
+     * Funcion para poder acceder al sistema
+     */
     public function login($username, $password){
         $modelLogin = new Auth();
         $rta = $modelLogin->login($username, $password);
@@ -10,5 +12,11 @@ class AuthController{
             $contenido = "Location: ./../views/dashboard.php";
             require "./views/home.php";
         }
+    }
+    /*** Logout
+     * Funcion para cerrar la sesion ***/
+    public function logout(){
+        session_destroy();
+        header("Location: ./../index.php");
     }
 }
