eb166abb4b5f2db268793a0e02f1536fe2fff2d9
ChopCode <87330304+Brandon094@users.noreply.github.com>
2026-03-13 22:11:56 -0500

    Se agrega el logo de YÁYA con un enlace para q redireccione al home.

--- views/navbar.php
+++ views/navbar.php
@@ -1,6 +1,9 @@
 <!-- NAVBAR -->
 <header class="navbar">
-    <div class="logo">MiPanel</div>
+    <div class="img-logo">
+        <a href="./../routers/routerUsers.php"><img src="./../public/img/logo.png" alt="Logo principal de YÁYA"></a>
+    </div>
+    <div class="logo">YÁYA</div>
     <div class="user-info">
         <span>Bienvenido, Usuario</span>
     </div>
