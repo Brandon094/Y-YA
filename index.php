72f8e24a689dfc4888fdc86749774cab07782231
ChopCode <87330304+Brandon094@users.noreply.github.com>
2026-03-17 18:50:39 -0500

    Agregar comentarios para describir la funcionalidad del archivo index.php

--- index.php
+++ index.php
@@ -2,8 +2,9 @@
 include_once './controllers/authController.php';
 /** Controlador de autenticación */
 if (isset($_POST['login'])) {
-    $auth = new AuthController();
-    $auth->login($_POST['username'], $_POST['password']);
+    $auth = new AuthController(); // Instancia del controlador de autenticación
+    $auth->login($_POST['username'], $_POST['password']); // Llama al método de login con los datos del formulario
 } else {
-    header("Location: ./views/login.php");
+    header("Location: ./views/login.php"); // Redirige a la página de login si no se ha enviado el formulario
+    exit();
 }
