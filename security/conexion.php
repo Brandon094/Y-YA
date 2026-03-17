c1d67be75f41fd392be4d9d7bb27aebdc171501b
ChopCode <87330304+Brandon094@users.noreply.github.com>
2026-03-17 18:14:33 -0500

    Se agregan comentarios para describir la funcionalidad de este archivo que se encarga de la conexion a la base de datos.

--- security/conexion.php
+++ security/conexion.php
@@ -1,20 +1,11 @@
 <?php
 function conectionDB(){
+    // Configuración de la conexión a la base de datos
     try {
-        $conn = new PDO("mysql:host=localhost;dbname=basedatos_yaya", "root", "");
-        return $conn;
-    } catch (PDOException $e) {
+        $conn = new PDO("mysql:host=localhost;dbname=db_yaya", "root", "");
+        return $conn; // Retorna la conexión establecida
+    } catch (PDOException $e) { // Manejo de errores de conexión
         echo "Error de conexión a la base de datos.";
-        return null;
+        return null; // Retorna null en caso de error
     }
 }
-
-/**
-$host = "localhost";
-$db = "basedatos_yaya";
-$user = "root";
-$pass = "";
-$charset = "utf8mb4";
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$conexion = new PDO($dsn, $user, $pass);
- */
