f10495519ffa4323e2d7af92f14ec60b28fd751a
ChopCode <87330304+Brandon094@users.noreply.github.com>
2026-03-18 16:25:57 -0500

    Se agregan comentarios para describir cada caso

--- routers/routerUsers.php
+++ routers/routerUsers.php
@@ -7,11 +7,11 @@ $x = $_REQUEST['X'] ?? null;
 // Define el contenido según el parámetro X
 switch ($x) {
     case '1':
-        include(__DIR__ . "/../views/formRegisterUsers.php");
+        include(__DIR__ . "/../views/formRegisterUsers.php"); // Muestra la vista de registro de usuarios
         break;
     case '2':
-        $contenido = "./../views/formRegisterServices.php";
-        require_once(__DIR__ . '/../views/home.php');
+        $contenido = "./../views/formRegisterServices.php"; // Define el contenido para la vista de registro de servicios
+        require_once(__DIR__ . '/../views/home.php'); // Carga la vista principal (home.php) que incluirá el contenido definido anteriormente
         break;
     case '3':
         $auth = new AuthController(); // Instancia del controlador de autenticación
@@ -19,7 +19,7 @@ switch ($x) {
         include(__DIR__ . "/../views/logout.php"); // Muestra la vista de logout después de cerrar sesión
         break;
     default:
-        $contenido = "./../views/dashboard.php";
-        require_once(__DIR__ . '/../views/home.php');
+        $contenido = "./../views/dashboard.php"; // Define el contenido para la vista del dashboard (página principal después de iniciar sesión)
+        require_once(__DIR__ . '/../views/home.php'); // Carga la vista principal (home.php) que incluirá el contenido definido anteriormente
         break;
 }
