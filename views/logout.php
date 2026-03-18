127e96a4f2beee783fa05fe4407b39dcecaf8f41
ChopCode <87330304+Brandon094@users.noreply.github.com>
2026-03-17 21:57:20 -0500

    Se agrega una nueva vista para el logout y sus estilos correspondientes

---
+++ views/logout.php
@@ -0,0 +1,12 @@
+<link rel="stylesheet" href="./../public/css/style.css">
+<link rel="stylesheet" href="./../public/css/footer.css">
+<link rel="stylesheet" href="./../public/css/content.css">
+<link rel="stylesheet" href="./../public/css/logout.css">
+<div class="content">
+    <div class="content-logout">
+        <h1>Logout</h1>
+        <p>Has cerrado sesión exitosamente.</p>
+        <a href="./../routers/routerAuth.php">Volver al inicio</a>
+    </div>
+</div>
+<?php include __DIR__ . '/footer.php'; ?>
